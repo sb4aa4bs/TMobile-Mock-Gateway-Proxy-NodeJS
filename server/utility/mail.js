b5454691ce82dde7d9dc9073c7af0d94ffd45bdc
ujindal <ujindal@prokarma.com>
2017-02-20 12:18:29 -0600

    email info

--- server/utility/mail.js
+++ server/utility/mail.js
@@ -2,31 +2,38 @@
  * Created by rghosh on 2/20/2017.
  */
 const nodemailer = require('nodemailer');
+var smtpTransport = require('nodemailer-smtp-transport');
+
+/*var transport = nodemailer.createTransport(smtpTransport({
+    service: 'gmail',
+    auth: {
+        user: '*******@gmail.com',
+        pass: '*****password'
+    }
+}));*/
 
 module.exports= {
 
-    sendMail2Customer: function (order) {
+    sendMail2Customer: function (order, cart) {
         console.log('sendMail2Customer')
-        var transporter = nodemailer.createTransport({
+        var transporter = nodemailer.createTransport(smtpTransport({
             service: 'gmail',
             auth: {
                 user: 'robin.ghos@gmail.com',
                 pass: 'Panaji9091'
             }
-        });
+        }));
         var mailOptions = {
             to: 'rghosh@prokarma.com',
             subject: 'Your order complete with order id=' + order.id,
             text: "",
-            html: " <strong>Your email   </strong>" + order.email + "<br>"
-                  + " <strong>Your firstname:   </strong>" + order.firstname + "<br>" +
-                  + " <strong>Your lastname:   </strong>" + order.lastname + "<br>" +
-
-                /*
-                  Iterate the order and get just the name of the product and price, maybe total
-                 */
-                  + " Thank you for placing an order with us."
+            html: " <strong>Your email   </strong>" + order.email + "<br>" +
+                  " <strong>Your firstname:  </strong>" + order.firstname + "<br>" +
+                  " <strong>Your lastname:   </strong>" + order.lastname + "<br>" +
+                  " <strong>Total Price:     </strong>$" + cart.total + "<br>" +
+                   " Thank you for placing an order with us."
         };
+
         transporter.sendMail(mailOptions, function (error, info) {
             if (error) {
                 return console.log(error);
